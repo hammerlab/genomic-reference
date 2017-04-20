6d1db9b8e43de7c00c00dafac7d66818ac8ddcde
Ryan Williams <ryan.blake.williams@gmail.com>
2017-04-20 21:22:02 +0000

    rename Contig, set default reference apply impl

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 organization := "org.hammerlab.genomics"
 name := "reference"
-version := "1.2.4"
+version := "1.3.0-SNAPSHOT"
 
 enableMacroParadise
 
--- src/main/scala/org/hammerlab/genomics/reference/Contig.scala
+++ src/main/scala/org/hammerlab/genomics/reference/Contig.scala
@@ -2,9 +2,10 @@ package org.hammerlab.genomics.reference
 
 import org.hammerlab.genomics.bases.{ Base, Bases, BasesBuffer }
 
-trait ContigSequence extends BasesBuffer {
+trait Contig
+  extends BasesBuffer {
   override type LengthT = NumLoci
-  def apply(locus: Locus): Base
+  def apply(locus: Locus): Base = slice(locus, 1).head
   def slice(start: Locus, length: Int): Bases
   def length: NumLoci
 }
